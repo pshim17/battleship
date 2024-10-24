0e854cde56614df44ab63cbe3fc7b6407dad4678
Jackson Bickler <jbickler4@gmail.com>
2024-10-24 16:09:23 -0600

    ship class passing

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require'./lib/ship'
 
 RSpec.describe Ship do
   before(:each) do
@@ -7,34 +8,34 @@ RSpec.describe Ship do
 
   describe 'intialize' do
     it "exits with attributes" do
-      expect(@crusier.name).to be_a(String)
+      expect(@cruiser.name).to be_a(String)
       expect(@cruiser.name).to eq("Cruiser")
       expect(@cruiser.length).to eq(3)
-      expect(@crusier.length).to be_a(Integer)
-      expect(@crusier.health).to eq(3)
+      expect(@cruiser.length).to be_a(Integer)
+      expect(@cruiser.health).to eq(3)
     end
   end
 
   describe 'hit' do
     it "it subtracts 1 health point from ship" do
       expect(@cruiser.health).to eq(3)
-      @crusier.hit
+      @cruiser.hit
       expect(@cruiser.health).to eq(2)
-      @crusier.hit
+      @cruiser.hit
       expect(@cruiser.health).to eq(1)
-      @crusier.hit
+      @cruiser.hit
       expect(@cruiser.health).to eq(0)
     end
   end
   
   describe 'sunk?' do
     it 'tells if a ship has been sunk (has 0 health)' do
-      expect(@crusier.ship?).to eq(false)
-      @crusier.hit
-      @crusier.hit
-      expect(@crusier.ship?).to eq(false)
-      @crusier.hit
-      expect(@crusier.ship?).to eq(true)
+      expect(@cruiser.sunk?).to eq(false)
+      @cruiser.hit
+      @cruiser.hit
+      expect(@cruiser.sunk?).to eq(false)
+      @cruiser.hit
+      expect(@cruiser.sunk?).to eq(true)
     end
   end
 end
