c1b247247a697a0e10c04d1e9e1a7911995825b7
Patrick Shim <patrick.shim91@yahoo.com>
2024-10-25 15:45:30 -0400

    added render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -30,23 +30,23 @@ class Cell
    @fire_upon
   end
 
-  def render(show_ship = false)
-    if @fired_upon == false
+def render(show_ship = false)
+  if @fired_upon == false
       if @ship && @show_ship == true
-        return "S"
-      else
-        return "."
-      end
-    elsif @fire_upon
-      if @ship != nil
-        if @ship.sunk?
-          return "X"
-        else 
-          return "H"
+          return "S"
+        else
+          return "."
         end
-      else 
-        return "M"
+      elsif @fire_upon
+        if @ship != nil
+          if @ship.sunk?
+            return "X"
+          else 
+            return "H"
+          end
+        else 
+          return "M"
+        end
       end
     end
-  end
 end
