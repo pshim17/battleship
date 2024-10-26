e0ceca40432f06cd7bf9f0e88338d88cc5c08392
Jackson Bickler <jbickler4@gmail.com>
2024-10-26 16:45:56 -0600

    cells hash tested

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Board do
   describe "intiialize" do
     it "starts board off with cells" do
       expect(@board.cells).to be_a(Hash)
+      expect(@board.cells.keys.size).to eq(16)
+      expect(@board.cells.values.size).to eq(16)
+      expect(@board.cells["A1"]).to be_a(Cell)
+      expect(@board.cells["A5"]).to eq(nil)
     end
   end
 
